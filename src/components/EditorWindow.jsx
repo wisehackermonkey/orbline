e6bd3e8ea3a3dcb0b8ffebf76969660b237d46e0
Oran C <oranbusiness@gmail.com>
2022-01-16 20:36:25 -0700

    fix: formating

--- src/components/EditorWindow.jsx
+++ src/components/EditorWindow.jsx
@@ -1,27 +1,26 @@
-import React,{useRef} from "react";
-import ReactDOM from "react-dom";
+import React, { useRef } from "react";
 
 import Editor from "@monaco-editor/react";
 export default function EditorWindow() {
 
   const editorRef = useRef(null);
   function handleEditorDidMount(editor, monaco) {
-     editorRef.current = editor; 
-   }
-   
-   function showValue() {
-     alert(editorRef.current.getValue());
-   }
+    editorRef.current = editor;
+  }
+
+  function showValue() {
+    alert(editorRef.current.getValue());
+  }
   return (
     <div>
-   <Editor
-     height="90vh"
-     width="50vw"
-     defaultLanguage="javascript"
-     defaultValue="// some comment"
-     onMount={handleEditorDidMount}
-   />
-   <button onClick={showValue}>Show Value</button>
-   </div>
+      <Editor
+        height="90vh"
+        width="50vw"
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        onMount={handleEditorDidMount}
+      />
+      <button onClick={showValue}>Sync</button>
+    </div>
   );
 }
